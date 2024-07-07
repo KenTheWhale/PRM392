0460cfa6484c9a72191aeaf62e11350fb6787288
HuyTran0211 <quochuy2112002@gmail.com>
2024-07-07 23:21:52 +0700

    loginController

--- app/src/main/java/com/ken/pettradingplatform/SignInActivity.java
+++ app/src/main/java/com/ken/pettradingplatform/SignInActivity.java
@@ -1,9 +1,24 @@
 package com.ken.pettradingplatform;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ken.pettradingplatform.configurations.APIClientConfig;
+import com.ken.pettradingplatform.controllers.AccountController;
+import com.ken.pettradingplatform.reponses.LoginResponse;
+import com.ken.pettradingplatform.requests.LoginRequest;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignInActivity extends AppCompatActivity {
     @Override
@@ -11,5 +26,50 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
         setTitle("SIGN IN");
+
+        EditText etEmail = findViewById(R.id.et_email);
+        EditText etPassword = findViewById(R.id.et_password);
+        Button btnSignIn = findViewById(R.id.btn_signin);
+        TextView tvSignup = findViewById(R.id.tv_signup);
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        AccountController accountController = APIClientConfig.getClient().create(AccountController.class);
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(email, password, accountController);
+            }
+        });
+    }
+    private void login(String email, String password, AccountController accountController){
+        LoginRequest loginRequest = LoginRequest
+                .builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        Call<LoginResponse> response = accountController.login(loginRequest);
+
+        response.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                Toast.makeText(getApplicationContext(),response.body().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"Error! Login fail", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
