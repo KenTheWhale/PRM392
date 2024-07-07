89f0779331a58da73875a9b23d4df8d9a18f0a14
SE151443_TrieuAn <anttse151443@fpt.edu.vn>
2024-07-07 17:39:08 +0700

    user-view

--- app/src/main/java/com/ken/pettradingplatform/MainActivity.java
+++ app/src/main/java/com/ken/pettradingplatform/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ken.pettradingplatform;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button search, profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        setTitle("Pet Shop");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        search = findViewById(R.id.btn_search);
+        search.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchActivity.class));
+        });
+
+        profile = findViewById(R.id.btn_profile);
+        profile.setOnClickListener(v -> {
+            startActivity(new Intent(this, ProfileActivity.class));
         });
     }
 }
