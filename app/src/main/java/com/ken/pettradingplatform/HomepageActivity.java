ad124ba21e2353cfff6cb1f9b5c11344d4ccd2f6
HuyTran0211 <quochuy2112002@gmail.com>
2024-07-09 21:35:54 +0700

    add layout

--- app/src/main/java/com/ken/pettradingplatform/HomepageActivity.java
+++ app/src/main/java/com/ken/pettradingplatform/HomepageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomepageActivity extends Activity {
     @Override
---
+++ app/src/main/java/com/ken/pettradingplatform/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.ken.pettradingplatform;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
