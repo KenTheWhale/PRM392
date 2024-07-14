1b97d45a95271efb222ab1f596bd9bfe2c223d75
SE151443_TrieuAn <anttse151443@fpt.edu.vn>
2024-07-15 02:42:36 +0700

    done optimize ui

--- app/src/main/java/com/ken/pettradingplatform/HomepageActivity.java
+++ app/src/main/java/com/ken/pettradingplatform/HomepageActivity.java
@@ -2,14 +2,19 @@ package com.ken.pettradingplatform;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomepageActivity extends Activity {
+    ImageButton test;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        test = findViewById(R.id.test);
+        test.setOnClickListener(v-> test.setSelected(true));
     }
 }
