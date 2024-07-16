ca680933c3ba7ecb014569b0e12276b4f4ff5b3d
KenTheWhale <kienquoc11026789@gmail.com>
2024-07-16 11:24:13 +0700

    commit

--- app/src/main/java/com/ken/pettradingplatform/configurations/APIClientConfig.java
+++ app/src/main/java/com/ken/pettradingplatform/configurations/APIClientConfig.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClientConfig {
-    private static final String BASE_URL = "https://e2d2-116-110-42-100.ngrok-free.app/";
+    private static final String BASE_URL = "https://eafc-118-69-182-149.ngrok-free.app/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
